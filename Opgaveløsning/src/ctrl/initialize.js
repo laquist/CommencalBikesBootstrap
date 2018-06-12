6c117f3e30260d9324a1bc8b19e2a8d5766c26ed
Lars Lynnerup <lars9865@edu.campusvejle.dk>
2018-06-12 14:49:18 +0200

    Oprettet et par .js filer, uden så meget indhold

---
+++ Opgaveløsning/src/ctrl/initialize.js
@@ -0,0 +1,5 @@
+var root = {
+    model:{},
+    view:{},
+    ctrl:{}
+};
---
+++ Opgaveløsning/src/model/Bike.js
@@ -0,0 +1,14 @@
+class Bike {
+    /**
+     * Initializes a new instance of the Bike class.
+     * @param {string} name The name of the product
+     * @param {number} price The price of the product
+     * @param {string[]} colors The available colors
+     * @param {object} specs The specs of the bike.
+     */
+    // Lav dette om til at jeg hellere vil have et enkelt object, som så har alle disse værdier med sig
+    // I stedet for at give parametrene individuelt
+    constructor() {
+        
+    };
+};
